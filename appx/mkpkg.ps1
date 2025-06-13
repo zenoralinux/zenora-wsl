cedc1837035d55fd9dbe4d65d36f097e08fe256f
Zenora Linux <64502346+zenoralinux@users.noreply.github.com>
2025-06-13 13:22:20 -0700

    Delete appx directory

--- appx/mkpkg.ps1
+++
@@ -1,4 +0,0 @@
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\MakeAppx.exe" pack /d .\AppxPackage /p \release\app\Zenora_WSL_1.0.0.0_x64.appx
-
-
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /fd SHA256 /f certificate.pfx /p zenora1234 \release\app\Zenora_WSL_1.0.0.0_x64.appx
